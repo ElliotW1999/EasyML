44324a6568c1bfdd9fe51b4a60e0e204bcb93cd1
Elliot Warner <warner.e@windowslive.com>
2024-07-24 01:51:22 +1200

    Added a training page

---
+++ frontend/src/lib/components/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+  // Add any necessary script here
+  import { goto } from '$app/navigation';
+
+  function handleClick() {
+    goto('/train');
+  }
+</script>
+  
+<style>
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+  }
+</style>
+
+<button on:click={handleClick}>
+  Get started
+</button>
+  
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import Button from '$lib/components/Button.svelte';
 </script>
 
 <svelte:head>
@@ -18,14 +19,10 @@
 			</picture>
 		</span>
 
-		to your new<br />SvelteKit app
+		Something inspiring<br />with mAcHiNe LeArNiNg
 	</h1>
+	<Button />
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
 </section>
 
 <style>
