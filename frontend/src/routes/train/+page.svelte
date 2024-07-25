a730e717c2da81a77e74792722a4a4bf9860518e
Elliot Warner <warner.e@windowslive.com>
2024-07-25 18:27:44 +1200

    radio button advanced

--- frontend/src/routes/train/+page.svelte
+++ frontend/src/routes/train/+page.svelte
@@ -2,7 +2,15 @@
 	import FileUpload from '$lib/components/FileUpload.svelte';
 	import { Button, Dropdown, DropdownItem, Radio } from 'flowbite-svelte';
 	import { ChevronDownOutline } from 'flowbite-svelte-icons';
-	let group1 = 1;
+	// Define the options for the radio buttons
+	let options = [
+		{ value: 1, text: 'Table-based' },
+		{ value: 2, text: 'Time series' },
+		{ value: 3, text: 'Multimedia' }
+	];
+
+	// Bind the selected value to a variable
+	let group1 = options[0].value;
 </script>
 
 
@@ -31,7 +39,15 @@
 
 	Select which type of problem to solve:
 	<section>
-		<Button>Dropdown radio {group1} <ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" /></Button>
+		<Button> 
+			{#if group1 === 1}
+				Table-based
+			{:else if group1 === 2}
+				Time series
+			{:else if group1 === 3}
+				Multimedia
+			{/if}
+			<ChevronDownOutline value={group1} class="w-6 h-6 ms-2 text-white dark:text-white" /></Button>
 		<Dropdown class="w-48 p-3 space-y-1">
 		<li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
 			<Radio name="group2" bind:group={group1} value={1}>Table-based</Radio>
